d429642c1fa0a0f3e33ff8c306bd3dc3b22b115a
Calebe Oliveira <calebe81899026@gmail.com>
2024-04-19 14:30:02 -0300

    configurações do ambiente

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function Home(){
+    return(
+        <View className='flex-1 bg-green-500 items-center justify-center'>
+
+            
+            <Text className='text-white'>Ola, React Native</Text>
+        </View>
+    )
+}
